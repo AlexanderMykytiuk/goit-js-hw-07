f6c972b42809e40781e9156fd2891d2ef76fc7f3
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-11-14 01:55:43 +0200

    Поччав задачу №6

---
+++ JS/task-namber_6.js
@@ -0,0 +1,10 @@
+
+const inputRef = document.querySelector('#validation-input');
+
+inputRef.addEventListener('focus', e => {
+
+});
+
+inputRef.addEventListener('blur', e => {
+    console.log(e);
+});
