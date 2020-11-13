ed9cc631ddb9aca5457fd2ced9b2cbdba0ee49d1
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-11-13 15:14:43 +0200

    Задача №1 модернізував

--- JS/task-namber_1.js
+++ JS/task-namber_1.js
@@ -2,18 +2,7 @@ const categoriesRef = document.querySelectorAll('.item');
 
 console.log(`В списке ${categoriesRef.length} категории.`);
 
-categoriesRef.forEach(element => {categoriesRef 
-    const titleRef = element.querySelector('h2');
-    const listRef = element.querySelector('ul');
-    const lengthRef = listRef.children;
-    console.log(`Категория: ${titleRef.textContent}` );
-    console.log(`Количество элементов:${lengthRef.length}`);
+categoriesRef.forEach(element => {
+    console.log(`Категория: ${element.querySelector('h2').textContent}`);
+    console.log(`Количество элементов:${element.querySelector('ul').children.length}`);
 });
-
-// categoriesEL.forEach(element => {
-//     // const listEL = element.querySelectorAll("li");
-//     // const nameOfCategoriesEL = element.querySelector("h2");
-//     console.log(`
-//  - Категория: ${element.querySelector("h2").textContent}
-//  - Количество элементов: ${element.querySelectorAll("li").length}`);
-// })
