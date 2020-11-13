350a98c1297bf759cf3f148f9b093020839adc72
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-11-13 15:06:59 +0200

    Задача №1 закінчив

--- JS/task-namber_1.js
+++ JS/task-namber_1.js
@@ -1,3 +1,19 @@
+const categoriesRef = document.querySelectorAll('.item');
 
-const categoriesRef = document.querySelector('.item');
-console.log(categoriesRef);
+console.log(`В списке ${categoriesRef.length} категории.`);
+
+categoriesRef.forEach(element => {categoriesRef 
+    const titleRef = element.querySelector('h2');
+    const listRef = element.querySelector('ul');
+    const lengthRef = listRef.children;
+    console.log(`Категория: ${titleRef.textContent}` );
+    console.log(`Количество элементов:${lengthRef.length}`);
+});
+
+// categoriesEL.forEach(element => {
+//     // const listEL = element.querySelectorAll("li");
+//     // const nameOfCategoriesEL = element.querySelector("h2");
+//     console.log(`
+//  - Категория: ${element.querySelector("h2").textContent}
+//  - Количество элементов: ${element.querySelectorAll("li").length}`);
+// })
