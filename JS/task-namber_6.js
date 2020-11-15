d9cb794dacb5fdbee7320b7b1439b843e8cb5a5c
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-11-15 15:01:43 +0200

    Проблема

--- JS/task-namber_6.js
+++ JS/task-namber_6.js
@@ -2,10 +2,10 @@
 const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', e => {
-    e.target.value.length <= 6 
+    e.target.value.length === 6 
     ? inputRef.classList.add('valid') 
     : inputRef.classList.add('invalid')
-    console.log(e);
+    
 });
 
 inputRef.addEventListener('focus', e => {
