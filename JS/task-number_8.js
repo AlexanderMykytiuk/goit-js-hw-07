b298e5f2c50c4077d264c114b959b2b0c3602a95
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-11-17 14:12:49 +0200

    Задачу 8 виконав

--- JS/task-number_8.js
+++ JS/task-number_8.js
@@ -1,38 +1,35 @@
 
-const renderRef = document.querySelector('button[data-action="render"]');
-const destroyRef = document.querySelector('button[data-action="destroy"]');
-const numberRef = document.querySelector('input');
+const renderRef = document.querySelector('[data-action="render"]');
+const destroyRef = document.querySelector('[data-action="destroy"]');
 const boxRef = document.querySelector('#boxes');
 
-// console.log(renderRef);
-// console.log(destroyRef);
-// console.log(numberRef);
-// console.log(boxRef);
-const submitInput = document.addEventListener('click', onCreateBoxes());
-const clinPage = document.addEventListener('click', onDestroyBoxes());
+renderRef.addEventListener('click', getRender);
+destroyRef.addEventListener('click', onDestroyBoxes);
 
-
-function onCreateBoxes(amount) {
-    for(let i = 0; i < amount; i += 1){
-        let makersDiv = document.createElement('div');
-        makersDiv.setAttribute('style', `width: ${30+i*10}px; height:${30+i*10}px; 
-        background-color: ${randomBoxColor()}`);
-        numberRef.boxRef.appendChild(makersDiv);
-    }
+function getRender() {
+    let amount = document.querySelector('#controls input').value;
+    onCreateBoxes(amount);
 };
 
 function  onDestroyBoxes() {
-
+    boxRef.innerHTML = '';
 };
 
-function randomBoxColor() {
-    const x = Math.floor(Math.random() * 256);
-    const y = Math.floor(Math.random() * 256);
-    const z = Math.floor(Math.random() * 256);
-    const boxColor = `rgb(${x},${y},${z})`;
-  
-    makersDiv.style.color = boxColor;
+ function randomBoxColor() {
+    return Math.floor(Math.random() * 256);
+};
+ 
+function onCreateBoxes(amount) {
+    let basicSize = 30;
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < amount; i += 1) {
+        let size = basicSize + i * 10;
+        let makersDiv = document.createElement('div');
+        makersDiv.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgb( ${randomBoxColor()} , ${randomBoxColor()} , ${randomBoxColor()} )`;
+        fragment.appendChild(makersDiv);
+       
     }
-    // randomBoxColor();
-
-console.log(onCreateBoxes(numberRef.textContent));
+        boxRef.appendChild(fragment);
+       
+};
+ 
